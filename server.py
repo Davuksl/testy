7dbb364f2144698df5205c937839cc50387fca4f
Davuks <skibidilol553@gmail.com>
2025-08-07 18:50:07 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import socket
+import os
 
 HOST = '0.0.0.0'
-PORT = 12345
+PORT = int(os.environ.get('PORT', 12345))  # получаем порт от Render, иначе fallback на 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
-    print("Waiting for connection...")
+    print(f"Listening on {HOST}:{PORT}...")
 
-    while True:  # Обработка новых подключений без выхода
+    while True:
         conn, addr = s.accept()
         print(f"Connected by {addr}")
         with conn:
